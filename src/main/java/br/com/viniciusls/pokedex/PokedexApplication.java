ffcd662b017e8d5a5cb8649dacf6ab80d23e0969
Vinicius Silva <vinicius.silva@ibm.com>
2020-09-03 17:12:28 -0300

    Added flux logic + mongoDB init logic to application init

--- src/main/java/br/com/viniciusls/pokedex/PokedexApplication.java
+++ src/main/java/br/com/viniciusls/pokedex/PokedexApplication.java
@@ -1,7 +1,13 @@
 package br.com.viniciusls.pokedex;
 
+import br.com.viniciusls.pokedex.model.Pokemon;
+import br.com.viniciusls.pokedex.repository.PokedexRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class PokedexApplication {
@@ -10,4 +16,19 @@ public class PokedexApplication {
 		SpringApplication.run(PokedexApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(ReactiveMongoOperations operations, PokedexRepository repository) {
+		return args -> {
+			Flux<Pokemon> pokedexFlux = Flux.just(
+					new Pokemon(null, "Bulbassaur", "Leaf", "Over Grow", 6.09),
+					new Pokemon(null, "Charizard", "Fire", "Blaze", 90.05),
+					new Pokemon(null, "Caterpie", "Worm", "Shell dust", 2.09),
+					new Pokemon(null, "Blastoise", "Water", "Flux", 6.09)
+			).flatMap(repository::save);
+
+			pokedexFlux
+					.thenMany(repository.findAll())
+					.subscribe(System.out::println);
+		};
+	}
 }
